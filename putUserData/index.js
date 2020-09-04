388ec93421a22ff898fdb6d3cadd485bffb63eee
Sara Fotros <sarafotros@gmail.com>
2020-09-04 12:13:59 +0100

    folders

---
+++ putUserData/index.js
@@ -0,0 +1,42 @@
+'use strict'
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'eu-west-2'})
+
+exports.handler = async (event, context)=> {
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'})
+
+    let responseBody = '';
+    let statusCode = 0 ;
+
+    const { id, firstname , lastname} = JSON.parse(event.body) 
+
+    const params = {
+        TableName: "Users",
+        Item:{
+            id: id,
+            firstname:firstname,
+            lastname: lastname
+        }
+    }
+
+    try{
+      const data= await  documentClient.put(params).promise()
+      responseBody = JSON.stringify(data)
+      statusCode = 201
+    }catch(err){
+        responseBody = `Unable to put user data`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers:{
+            "myHeader": "test"
+        },
+        body: responseBody
+    }
+
+    return response;
+}
