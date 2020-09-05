4b1ccc72e70c6ba16d5fdc4ee598cdf7868d9292
Sara Fotros <sarafotros@gmail.com>
2020-09-05 19:07:25 +0100

    lambda functions updated

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require("./server")
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...!!!!`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
--- putUserData/index.js
+++ putUserData/index.js
@@ -1,42 +1,85 @@
-'use strict'
+
+'use strict';
 const AWS = require('aws-sdk');
 
-AWS.config.update({region: 'eu-west-2'})
+AWS.config.update({region: 'eu-west-2'});
 
 exports.handler = async (event, context)=> {
-    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'})
+    // const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+    const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
 
     let responseBody = '';
     let statusCode = 0 ;
 
-    const { id, firstname , lastname} = JSON.parse(event.body) 
+    const { id, email} = JSON.parse(event.body);
 
     const params = {
-        TableName: "Users",
+        TableName: "Subscribers",
         Item:{
-            id: id,
-            firstname:firstname,
-            lastname: lastname
+            id:id,
+            email:email
         }
-    }
+    };
 
     try{
-      const data= await  documentClient.put(params).promise()
-      responseBody = JSON.stringify(data)
-      statusCode = 201
+      const data= await  documentClient.put(params).promise();
+
+      responseBody = JSON.stringify({...data, "message":"Successfully added to DB" })
+      statusCode = 201;
     }catch(err){
-        responseBody = `Unable to put user data`;
+        responseBody = `Unable to put subscriber email`;
         statusCode = 403;
     }
 
     const response = {
         statusCode: statusCode,
         headers:{
-            "myHeader": "test"
+            
         },
         body: responseBody
     }
-
+;
     return response;
-}
+};
+// 'use strict'
+// const AWS = require('aws-sdk');
+
+// AWS.config.update({region: 'eu-west-2'})
+
+// exports.handler = async (event, context)=> {
+//     const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08"});
+//     const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'})
+
+//     let responseBody = '';
+//     let statusCode = 0 ;
+
+//     const { id, firstname , lastname} = JSON.parse(event.body) 
+
+//     const params = {
+//         TableName: "Users",
+//         Item:{
+//             id: id,
+//             firstname:firstname,
+//             lastname: lastname
+//         }
+//     }
+
+//     try{
+//       const data= await  documentClient.put(params).promise()
+//       responseBody = JSON.stringify(data)
+//       statusCode = 201
+//     }catch(err){
+//         responseBody = `Unable to put user data`;
+//         statusCode = 403;
+//     }
+
+//     const response = {
+//         statusCode: statusCode,
+//         headers:{
+//             "myHeader": "test"
+//         },
+//         body: responseBody
+//     }
+
+//     return response;
+// }
